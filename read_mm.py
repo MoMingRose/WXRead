70dda82e9fa8a0d98bd219fc46dfb7d7dfa63848
MoMingLog <108046002+MoMingRose@users.noreply.github.com>
2024-04-03 18:45:01 +0800

    修改注释内容

--- read_mm.py
+++ read_mm.py
@@ -7,7 +7,7 @@
 new Env("猫猫看看");
 0 30 7-23 * * * read_mm.py
 
-统一入口链接：http://9pw4.dsdtew.shop/haobaobao/auth/c5aab76cbaa0d0c80ec1ade47b3ce520
+统一入口链接：http://72484f04031828d9e5.kgiuitq.cn/haobaobao/auth/f5097609e2ff70f696af4c1ed8b3ed4e
 
 如果进不去，可以先运行一下 “read_entry_url.py”，如果青龙任务添加成功，应该称为 “阅读入口”
 
